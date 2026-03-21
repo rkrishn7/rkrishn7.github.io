ba3569d1ef2d6b94cf943dfef502eb7a4c29062c
Rohan Krishnaswamy <47869999+rkrishn7@users.noreply.github.com>
2026-03-20 17:24:50 -0700

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+
+export default defineConfig({
+  site: "https://rohank.me",
+  integrations: [tailwind(), sitemap(), mdx()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+    },
+  },
+});
